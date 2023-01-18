0dbbbe8ff83eddccf3384279b48c63392751ac49
Romit Dobariya <97334928+romit5075@users.noreply.github.com>
2023-01-18 16:47:56 +0530

    Create next.java

---
+++ next.java
@@ -0,0 +1,52 @@
+package com.packege;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class exam extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Welcome exam servlet</h1>");
+		
+		String name=request.getParameter("name");
+		out.println("Welcome  "+name);
+		
+	
+		String name=request.getParameter("name");
+		String email =request.getParameter("email");
+		String gender=request.getParameter("gender");
+		String course=request.getParameter("course");
+		String tc=request.getParameter("tc");
+		
+		if(tc!=null) {
+			if(tc.equals("checked")) {
+				out.println("<h2> Name : " +name+ "</h2>");
+				out.println("<h2> Email : " +email+ "</h2>");
+				out.println("<h2> Gender : " +gender+ "</h2>");
+				out.println("<h2> course : " +course+ "</h2>");
+				
+//			Cookie create (data save in cookie)	
+				
+				
+			}else {
+				out.println("<h2>You have Not Acceptyed T&C</h2>");
+			}
+		}else {
+			out.println("<h2>You have Not Acceptyed T&C</h2>");
+		}
+		
+	}
+	
+	
+}
